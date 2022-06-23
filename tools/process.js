d7f07033608de911a93c23f4448a8d0a78bd240b
Maria Eduarda de S. Barros <maria123duarda@gmail.com>
2022-06-23 17:58:27 -0300

    eventos e tools

---
+++ tools/process.js
@@ -0,0 +1,31 @@
+require('./subdirectory/sub');
+
+console.log('----------------------------------------------');
+console.log('Nome do Arquivo:', __filename);
+console.log('Diretório do Arquivo:', __dirname);
+console.log('Diretório em que foi invocado: ', process.cwd());
+//console.log('Parâmetros de execução:' , process.argv); //array de parâmetros
+//console.log('Parâmetros de execução: ', process.env);
+console.log('Sistema Operacional: ', process.env.OS);
+console.log('Usuário no SO: ', process.env.USERNAME);
+console.log('Idioma: ', process.env.LANG);
+console.log('Nome do Server: ', process.env.COMPUTERNAME);
+
+switch(process.argv[2]){
+
+    case '-a':
+        console.log('Execute rotina principal');
+        break;
+
+    case '-i':
+        console.log('Execute instalação');
+        break;
+
+    case '-q':
+        console.log('Encerrando aplicação');
+        process.exit(); //interrompe imediatamente o código
+        
+    default:
+        console.log('Parâmetro inválido!')
+}
+console.log(`Ambiente do Servidor: ${process.platform}`);
