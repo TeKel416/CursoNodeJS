d07db0588cf902ed684660dce649168ddce1d8e4
Maria Eduarda de S. Barros <maria123duarda@gmail.com>
2022-06-30 14:35:33 -0300

    att path

--- path/path.js
+++ path/path.js
@@ -13,7 +13,7 @@ console.log('Join: ' + (path.join('/teste', 'teste2', 'teste3/teste4', 'dir2', '
 // mandar um arquivo e receber o caminho absoluto dele
 // se o arquivo não existir, ele mostra o caminho absoluto a partir de onde o código está sendo executado
 // obs: isso tá meio errado: https://nodejs.dev/learn/the-nodejs-path-module
-console.log('Resolve: ' + (path.resolve('path.js')));
+console.log('Resolve: ' + (path.resolve('fs-sync.js')));
 
 // extensão do nome do arquivo
 console.log('Extensão: ' + (path.extname('path.js')));
